abae70c5fa2f9037f509bef506f2854c18141bad
Abhishek Kumar <abhishekism0010@gmail.com>
2023-11-09 02:35:05 +0530

    Rectified for docker dev versions

--- app/config/settings.py
+++ app/config/settings.py
@@ -129,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://django.abhishek0010.in'
+]
